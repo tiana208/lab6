3ea6465cac954c67a60326faa2491cb4955587fa
tiana <33078596+tiana208@users.noreply.github.com>
2017-11-09 02:54:37 -0500

    changes
    changes somehow

--- code/game.js
+++ code/game.js
@@ -1,3 +1,7 @@
+var actorChars = {
+	'@': Player,
+	'o': Coin,
+};
 function Level(plan) {
   // Use the length of a single row to set the width of the level
   this.width = plan[0].length;
@@ -7,6 +11,7 @@ function Level(plan) {
 
   // Store the individual tiles in our own, separate array
   this.grid = [];
+  this.actors = [];
 
   // Loop through each row in the plan, creating an array in our grid
   for (var y = 0; y < this.height; y++) {
@@ -19,9 +24,9 @@ function Level(plan) {
 
       var ch = line[x], fieldType = null;
       // Use if and else to handle the three cases
-      if (ch==='@')
-        // Create a new player at that grid position.
-        this.player = new Player(new Vector(x, y));
+     var Actor = actorChars[ch];
+	 if (Actor)
+		 this.actors.push(new Actor(new Vector(x,y),ch));
       else if (ch == "x")
         fieldType = "wall";
       // Because there is a third case (space ' '), use an "else if" instead of "else"
@@ -34,7 +39,17 @@ function Level(plan) {
     // Push the entire row onto the array of rows.
     this.grid.push(gridLine);
   }
+  this.player = this.actors.filter(function(actor) {
+		return actor.type == 'player';
+  })[0];
 }
+
+function Coin(pos) {
+	this.basePos = this.pos = pos.plus(new Vector(0.2,0.1));
+	this.size = new Vector (0.6, 0.6);
+	this.wobble = Math.random() * Math.PI * 2;
+}
+Coin.prototype.type='coin';
 
 function Vector(x, y) {
   this.x = x; this.y = y;
@@ -102,25 +117,26 @@ DOMDisplay.prototype.drawBackground = function() {
   return table;
 };
 
-// Draw the player agent
-DOMDisplay.prototype.drawPlayer = function() {
+// Draw the actors
+DOMDisplay.prototype.drawActor = function() {
   // Create a new container div for actor dom elements
   var wrap = elt("div");
-
-  var actor = this.level.player;
-  var rect = wrap.appendChild(elt("div",
+	
+  this.level.actors.forEach(function(actor) {  
+	var rect = wrap.appendChild(elt("div",
                                     "actor " + actor.type));
-  rect.style.width = actor.size.x * scale + "px";
-  rect.style.height = actor.size.y * scale + "px";
-  rect.style.left = actor.pos.x * scale + "px";
-  rect.style.top = actor.pos.y * scale + "px";
+	rect.style.width = actor.size.x * scale + "px";
+	rect.style.height = actor.size.y * scale + "px";
+	rect.style.left = actor.pos.x * scale + "px";
+	rect.style.top = actor.pos.y * scale + "px";
+  });
   return wrap;
 };
 
 DOMDisplay.prototype.drawFrame = function() {
   if (this.actorLayer)
     this.wrap.removeChild(this.actorLayer);
-  this.actorLayer = this.wrap.appendChild(this.drawPlayer());
+  this.actorLayer = this.wrap.appendChild(this.drawActors());
   this.scrollPlayerIntoView();
 };
 
@@ -169,19 +185,40 @@ Level.prototype.obstacleAt = function(pos,size) {
 	}
 };
 
+
+Level.prototype.actorAt = function(actor) {
+	for (var i=0; i<this.actors.length; i++) {
+		var other = this.actors[i];
+		if (other != actor && 
+			actor.pos.x + actor.size.x > other.pos.x &&
+			actor.pos.x < other.pos.x + other.size.x &&
+			actor.pos.y + actor.size.y > other.pos.y &&
+			actor.pos.y < other.pos.y + other.size.y)
+			return other;
+	}
+};
 // Update simulation each step based on keys & step size
 Level.prototype.animate = function(step, keys) {
 
   // Ensure each is maximum 100 milliseconds 
   while (step > 0) {
     var thisStep = Math.min(step, maxStep);
-      this.player.act(thisStep, this, keys);
+	this.actors.forEach(function(actor) {
+		actor.act(thisStep, this, keys);
+	}, this);
    // Do this by looping across the step size, subtracing either the
    // step itself or 100 milliseconds
     step -= thisStep;
   }
 };
 
+var wobbleSpeed=8;
+var wobbleDist = 0.07;
+Coin.prototype.act = function(step) {
+	this.wobble += step * wobbleSpeed;
+	var wobblePos = Math.sin(this.wobble) * wobbleDist;
+	this.pos = this.basePos.plus(new Vector (0, wobblePos));
+};
 var maxStep = 0.05;
 
 var playerXSpeed = 7;
@@ -224,8 +261,18 @@ Player.prototype.moveY = function(step, level, keys) {
 Player.prototype.act = function(step, level, keys) {
   this.moveX(step, level, keys);
   this.moveY(step, level, keys);
+  
+  var otherActor = level.actorAt(this);
+  if (otherActor)
+	  level.playerTouched(otherActor.type, otherActor);
 };
 
+Level.prototype.playerTouched = function(type, actor) {
+	if (type == 'coin'){
+		this.actors = this.actors.filter(function(other) {
+			return other !=actor;
+	});
+};
 
 // Arrow key codes for readibility
 var arrowCodes = {37: "left", 38: "up", 39: "right", 40: "down"};
@@ -295,4 +342,4 @@ function runGame(plans, Display) {
     runLevel(new Level(plans[n]), Display);
   }
   startLevel(0);
-}
+};
